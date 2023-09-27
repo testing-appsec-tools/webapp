a8ea4a6e93aaceb6cd949435a9928e0213de5af9
Liron Biam <129161216+LironJit@users.noreply.github.com>
2023-09-27 20:15:07 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,35 @@
+# Remediations for 7 vulnerabilities
+
+
+# /python.pyramid.security.sqlalchemy-sql-injection.pyramid-sqlalchemy-sql-injection.json
+from pyramid.view import view_config
+from sqlalchemy import func
+from myapp.models import MyModel
+
+
+@view_config(route_name='example', renderer='json')
+def example_view(request):
+    user_input = request.params.get('user_input')
+
+    query = request.dbsession.query(MyModel)
+    query = query.filter("column_name = '{}'".format(user_input))
+
+    results = query.all()
+
+    return {'results': results}
+
+
+# /python.lang.security.audit.subprocess-shell-true.subprocess-shell-true.json
+
+import subprocess
+output = subprocess.check_output(f"nslookup2 {mydomain}", shell=True, encoding='UTF-8')
+
+
+# /python.jwt.security.unverified-jwt-decode.unverified-jwt-decode.json
+import jwt
+token = 'your_jwt_token'
+decoded_token = jwt.decode(token, verify=False)
+
+# /python.lang.security.unverified-ssl-context.unverified-ssl-context.json
+import ssl
+context = ssl._create_unverified_context()  # Triggers the rule
